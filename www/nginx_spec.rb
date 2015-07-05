905f239a0e6bd4eedf6f6ebcb426d1479989b0e3
takenakasuji <t.nksj.e.89@gmail.com>
2015-07-05 13:11:17 +0000

    add describe file in nginx_spec.rb

--- www/nginx_spec.rb
+++ www/nginx_spec.rb
@@ -13,6 +13,10 @@ describe port(80) do
   it { should be_listening }
 end
 
+describe file('/etc/nginx/conf.d/nginx.conf') do
+  it { should be_file }
+end
+
 #describe file('/etc/httpd/conf/httpd.conf') do
 #  it { should be_file }
 #  it { should contain "ServerName www.example.jp" }
